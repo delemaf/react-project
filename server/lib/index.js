1ab6f42d8cb877becadded4ed1baf8e44b8cd941
Grisha GHUKASYAN <grisha@brigad.co>
2018-03-22 14:54:19 +0100

    fix: add gitignore

---
+++ server/lib/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import levelup from "levelup";
+import leveldown from "leveldown";
+
+const app = express();
+const port = parseInt(process.env.PORT) || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.info(`Lisening on ${port}...`);
+});
