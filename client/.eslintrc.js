31b725d6814f3d9ff597194bc19c61954f9c13e4
Florian Delemarre <florian.delemarre@epitech.eu>
2018-03-23 11:51:30 +0100

    Update eslint and package

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,1 +1,20 @@
-../.eslintrc.js
+module.exports = {
+  parser: 'babel-eslint',
+  extends: ['airbnb', 'prettier', 'prettier/react', 'plugin:react/recommended'],
+  env: {
+    node: true,
+    jest: true,
+    jasmine: true,
+    browser: true,
+  },
+  rules: {
+    'arrow-parens': ['error', 'as-needed', { requireForBlockBody: true }],
+    'consistent-return': 'error',
+    'operator-linebreak': ['error', 'before'],
+    'no-console': 'off',
+    strict: 'off',
+    'import/prefer-default-export': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/prefer-stateless-function': [0, { ignorePureComponents: true }],
+  },
+};
